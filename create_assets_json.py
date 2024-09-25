5b5c728969c098196e04db9f624f480219d642c6
Mike Christopher <mchristopher@users.noreply.github.com>
2024-09-25 08:10:09 -0700

    fix: pr feedback

--- create_assets_json.py
+++ create_assets_json.py
@@ -9,11 +9,6 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets.json'
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json'), 'r') as infile:
 	config = json.load(infile)
 
-def get_l1_network_config_from_fuel(networks, type, chain):
-	for network in networks:
-		if network.get('type') == type and network.get('chain') == chain:
-			return network
-
 def parse_address_to_bytes(address):
     if address.startswith('0x'):
         address = address[2:]  # Remove the '0x' prefix
@@ -23,6 +18,17 @@ def pad_address(address, byte_len):
 	hex_address = address.hex()
 	padded_hex_address = hex_address.zfill(byte_len)
 	return bytes.fromhex(padded_hex_address)
+
+def sha256(*data):
+	bytes_to_hash = bytearray()
+	for piece in data:
+		bytes_to_hash.extend(piece)
+	return hashlib.sha256(bytes_to_hash)
+
+def get_l1_network_config_from_fuel(networks, type, chain):
+	for network in networks:
+		if network.get('type') == type and network.get('chain') == chain:
+			return network
 
 def generate_fuel_network_config(asset, chain, network):
 	base_config = config['fuelAddressConfig'][chain]
@@ -46,26 +52,18 @@ def generate_fuel_network_config(asset, chain, network):
 def calculate_fuel_sub_id(chain_id, address):
 	# chain ID is always 1
 	# https://github.com/FuelLabs/fuel-bridge/blob/0b6e3b198707d9fd7000d255b0ad3164d7e64507/packages/fungible-token/bridge-fungible-token/implementation/src/main.sw#L496
-	chain_id = bytes('1'.encode('utf-8')) # number 1 in ASCII
+	chain_id = '1'.encode('utf-8') # chain ID in ASCII
 	token_id = bytes(32) # zeros, not used
 
 	# parse eth address and ensure padding
 	parsed_address = parse_address_to_bytes(address)
 	padded_address = pad_address(parsed_address, 64)
 
-	# setup bytes to sign
-	bytes_to_sign = bytearray(chain_id)
-	bytes_to_sign.extend(padded_address)
-	bytes_to_sign.extend(token_id)
-
-	return hashlib.sha256(bytes_to_sign)
+	return sha256(chain_id, padded_address, token_id)
 
 def calculate_fuel_asset_id(contract_id, sub_id):
 	parsed_contract_id = parse_address_to_bytes(contract_id)
-	bytes_to_sign = bytearray(parsed_contract_id)
-	bytes_to_sign.extend(bytes.fromhex(sub_id.hexdigest()))
-
-	return hashlib.sha256(bytes_to_sign)
+	return sha256(parsed_contract_id, sub_id.digest())
 
 local_data = []
 cdn_data = []
